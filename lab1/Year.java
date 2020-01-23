aa1329c12c217879c405f0a25b4e18f64d3d0f83
Pavel Gladkevich <pgladkevich@berkeley.edu>
2020-01-22 19:20:59 -0800

    Completed Year.java

--- lab1/Year.java
+++ lab1/Year.java
@@ -5,7 +5,9 @@ public class Year {
 
     /** Return true iff YEAR is a leap year.  */
     static boolean isLeapYear(int year) {
-        return true;    // TODO: YOUR CODE HERE
+        if ((year % 400 == 0) || ((year % 4 ==0) && (year % 100 != 0)){
+            return true;
+        }
     }
 
     /** Print whether YEAR is a a leap year on System.out. */

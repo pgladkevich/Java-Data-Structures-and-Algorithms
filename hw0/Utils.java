a77cb392105b312f1ecae0e5c6c628157dbd7af3
Pavel Gladkevich <pgladkevich@berkeley.edu>
2020-01-29 21:11:04 -0800

    Started 3SUM

--- hw0/Utils.java
+++ hw0/Utils.java
@@ -4,6 +4,11 @@
 
 public class Utils {
 
+	/** Class that find the maximum of an integer array
+	 * Either via a while loop or a for loop
+	 * @param a
+	 * @return greatest integer
+	 */
 	public static int max (int[] a) {
 		int len_a = a.length;
 		if (len_a > 0) {
@@ -31,8 +36,32 @@ public class Utils {
 		return 0;
 	}
 
+	/** Class that determines three integers (not neccesarily distinct) in a[] whose sum is zero.
+	 *
+	 * @param a
+	 * @return boolean
+	 */
 	public static boolean threeSum (int [] a) {
-		return false;
+
+		public static boolean checkForI
+
+		int len_a = a.length;
+
+		for (int i = 0; i < len_a; i+=1) {
+			if (a[i] == 0) {
+				return true;
+			}
+			else if (a[i] == ) {
+
+			}
+			else if () {
+
+			}
+			else {
+				return false;
+			}
+		}
+		//return false;
 	}
 
 	public static boolean threeSumDistinct (int [] a) {

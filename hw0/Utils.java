1d9b81cf99969624ef79b322c9d9fe5a640bf4bd
Pavel Gladkevich <pgladkevich@berkeley.edu>
2020-01-29 20:02:33 -0800

    Working on hw0

--- hw0/Utils.java
+++ hw0/Utils.java
@@ -5,7 +5,20 @@
 private class Utils {
 
 	public static int Max (int [] a) {
-		while 
+		len_a = a.length ()
+		if (len_a > 0) {
+			if (len_a % 2 == 0) {
+				k = a.length ()
+				while () {
+
+				}
+			}
+			else {
+				
+			}
+		}
+		
+			
 	}
 
 	public static boolean threeSum (int [] a) {

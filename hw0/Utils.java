c5deb1c4d075721dc763ace15a6618c5d4a1b4a6
Pavel Gladkevich <pgladkevich@berkeley.edu>
2020-01-29 20:17:16 -0800

    Updated max

--- hw0/Utils.java
+++ hw0/Utils.java
@@ -8,17 +8,28 @@ private class Utils {
 		len_a = a.length ()
 		if (len_a > 0) {
 			if (len_a % 2 == 0) {
-				k = a.length ()
-				while () {
-
+				int index = 0;
+				greatest = 0
+				while (index < len_a) {
+					if (a[index] >= greatest) {
+						greatest = a[index];
+					}
+					index += 1;
 				}
 			}
 			else {
-				
+				for (int index = 0; index < len_a; index +=1) {
+					if (a[index] >= greatest) {
+						greatest = a[index];
+					}
+				}
+				return greatest;
+
 			}
+		};
+		else{
+			return NullPointerException;
 		}
-		
-			
 	}
 
 	public static boolean threeSum (int [] a) {

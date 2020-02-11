3e4fa2565494a3d2c436f8f97bfb748e91b3e5b9
Pavel Gladkevich <pgladkevich@berkeley.edu>
2020-02-10 21:21:47 -0800

    Started doing hw2

--- hw2/lists/ListsTest.java
+++ hw2/lists/ListsTest.java
@@ -3,13 +3,15 @@ package lists;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/** FIXME
+/** Tests of the Lists class.
  *
- *  @author FIXME
+ *  @author Pavel Gladkevich
  */
 
 public class ListsTest {
-    /** FIXME
+    /** Test that the naturalRuns method properly breaks up the passed
+     * intList into an intListList that contains n intLists,
+     * each of which contains members strictly in ascending order.
      */
 
     // It might initially seem daunting to try to set up
@@ -18,6 +20,10 @@ public class ListsTest {
     // There is an easy way to get the IntListList that you want in just
     // few lines of code! Make note of the IntListList.list method that
     // takes as input a 2D array.
+    @Test
+    public void naturalRunsTest() {
+        Utils.readIntArray()
+    }
 
     public static void main(String[] args) {
         System.exit(ucb.junit.textui.runClasses(ListsTest.class));

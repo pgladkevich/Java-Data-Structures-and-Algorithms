19957c34a87566df4659ee0a1b5ab740999cd945
Pavel Gladkevich <pgladkevich@berkeley.edu>
2020-02-14 01:45:35 -0800

    Finished first part of hw2

--- hw2/lists/ListsTest.java
+++ hw2/lists/ListsTest.java
@@ -14,12 +14,6 @@ public class ListsTest {
      * each of which contains members strictly in ascending order.
      */
 
-    // It might initially seem daunting to try to set up
-    // IntListList expected.
-    //
-    // There is an easy way to get the IntListList that you want in just
-    // few lines of code! Make note of the IntListList.list method that
-    // takes as input a 2D array.
     @Test
     public void naturalRunsTest() {
         /* Some edge cases */
@@ -39,8 +33,6 @@ public class ListsTest {
     }
 
     public static void main(String[] args) {
-//        IntListList ending_L1 = new IntListList();
-//        ending_L1.list([[IntList.list(1,3,7)],[IntList.list(5)],[IntList.list(4,6)]])
         System.exit(ucb.junit.textui.runClasses(ListsTest.class));
     }
 }

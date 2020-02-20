8de6ff3291d56aa552b705fdc26a41025531472a
Paul Hilfinger <Hilfinger@cs.berkeley.edu>
2020-02-19 19:19:54 -0800

    Update proj1

---
+++ proj1/enigma/Main.java
@@ -0,0 +1,127 @@
+package enigma;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+import static enigma.EnigmaException.*;
+
+/** Enigma simulator.
+ *  @author
+ */
+public final class Main {
+
+    /** Process a sequence of encryptions and decryptions, as
+     *  specified by ARGS, where 1 <= ARGS.length <= 3.
+     *  ARGS[0] is the name of a configuration file.
+     *  ARGS[1] is optional; when present, it names an input file
+     *  containing messages.  Otherwise, input comes from the standard
+     *  input.  ARGS[2] is optional; when present, it names an output
+     *  file for processed messages.  Otherwise, output goes to the
+     *  standard output. Exits normally if there are no errors in the input;
+     *  otherwise with code 1. */
+    public static void main(String... args) {
+        try {
+            new Main(args).process();
+            return;
+        } catch (EnigmaException excp) {
+            System.err.printf("Error: %s%n", excp.getMessage());
+        }
+        System.exit(1);
+    }
+
+    /** Check ARGS and open the necessary files (see comment on main). */
+    Main(String[] args) {
+        if (args.length < 1 || args.length > 3) {
+            throw error("Only 1, 2, or 3 command-line arguments allowed");
+        }
+
+        _config = getInput(args[0]);
+
+        if (args.length > 1) {
+            _input = getInput(args[1]);
+        } else {
+            _input = new Scanner(System.in);
+        }
+
+        if (args.length > 2) {
+            _output = getOutput(args[2]);
+        } else {
+            _output = System.out;
+        }
+    }
+
+    /** Return a Scanner reading from the file named NAME. */
+    private Scanner getInput(String name) {
+        try {
+            return new Scanner(new File(name));
+        } catch (IOException excp) {
+            throw error("could not open %s", name);
+        }
+    }
+
+    /** Return a PrintStream writing to the file named NAME. */
+    private PrintStream getOutput(String name) {
+        try {
+            return new PrintStream(new File(name));
+        } catch (IOException excp) {
+            throw error("could not open %s", name);
+        }
+    }
+
+    /** Configure an Enigma machine from the contents of configuration
+     *  file _config and apply it to the messages in _input, sending the
+     *  results to _output. */
+    private void process() {
+        // FIXME
+    }
+
+    /** Return an Enigma machine configured from the contents of configuration
+     *  file _config. */
+    private Machine readConfig() {
+        try {
+            // FIXME
+            _alphabet = new Alphabet();
+            return new Machine(_alphabet, 2, 1, null);
+        } catch (NoSuchElementException excp) {
+            throw error("configuration file truncated");
+        }
+    }
+
+    /** Return a rotor, reading its description from _config. */
+    private Rotor readRotor() {
+        try {
+            return null; // FIXME
+        } catch (NoSuchElementException excp) {
+            throw error("bad rotor description");
+        }
+    }
+
+    /** Set M according to the specification given on SETTINGS,
+     *  which must have the format specified in the assignment. */
+    private void setUp(Machine M, String settings) {
+        // FIXME
+    }
+
+    /** Print MSG in groups of five (except that the last group may
+     *  have fewer letters). */
+    private void printMessageLine(String msg) {
+        // FIXME
+    }
+
+    /** Alphabet used in this machine. */
+    private Alphabet _alphabet;
+
+    /** Source of input messages. */
+    private Scanner _input;
+
+    /** Source of machine configuration. */
+    private Scanner _config;
+
+    /** File for encoded/decoded messages. */
+    private PrintStream _output;
+}
---
+++ proj1/enigma/MovingRotor.java
@@ -0,0 +1,29 @@
+package enigma;
+
+import static enigma.EnigmaException.*;
+
+/** Class that represents a rotating rotor in the enigma machine.
+ *  @author
+ */
+class MovingRotor extends Rotor {
+
+    /** A rotor named NAME whose permutation in its default setting is
+     *  PERM, and whose notches are at the positions indicated in NOTCHES.
+     *  The Rotor is initally in its 0 setting (first character of its
+     *  alphabet).
+     */
+    MovingRotor(String name, Permutation perm, String notches) {
+        super(name, perm);
+        // FIXME
+    }
+
+    // FIXME?
+
+    @Override
+    void advance() {
+        // FIXME
+    }
+
+    // FIXME: ADDITIONAL FIELDS HERE, AS NEEDED
+
+}

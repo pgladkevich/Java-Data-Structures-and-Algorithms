57fc177de4aa0111299c50fd7d6d87a011feff31
Pavel Gladkevich <pgladkevich@berkeley.edu>
2020-02-29 23:47:29 -0800

    Removed exception tests

--- lab6/enigma/PermutationTest.java
+++ lab6/enigma/PermutationTest.java
@@ -94,6 +94,9 @@ public abstract class PermutationTest {
         Permutation p3 = getNewPermutation("(1?)",
                 getNewAlphabet("1?0R"));
         assertEquals(4, p3.size());
+
+        Permutation p4 = getNewPermutation("", getNewAlphabet(""));
+        assertEquals(0, p4.size());
     }
 
     @Test
@@ -194,49 +197,49 @@ public abstract class PermutationTest {
                 getNewAlphabet("ABCX0"));
         assertEquals(false, p4.derangement());
     }
-    @Test(expected = IllegalArgumentException.class)
-    public void testNoAlphabet() {
-        Permutation p = getNewPermutation("",
-                getNewAlphabet(""));
-        if (p.size()==0) {
-            throw new IllegalArgumentException("No alphabet.");
-        }
-
-    }
-    @Test(expected = EnigmaException.class)
-    public void testNotInAlphabet() {
-        Permutation p = getNewPermutation("(ABCD)",
-                getNewAlphabet("ABC"));
-
-    }
-
-    @Test(expected = EnigmaException.class)
-    public void testInAlphabetTwice() {
-        Permutation p = getNewPermutation("(ABD)",
-                getNewAlphabet("ABBD"));
-
-    }
-    @Test(expected = EnigmaException.class)
-    public void testInCyclesTwice() {
-        Permutation p = getNewPermutation("(ABBD)",
-                getNewAlphabet("ABD"));
-
-    }
-    @Test(expected = IllegalArgumentException.class)
-    public void testInvalidLetter() {
-        Alphabet a = getNewAlphabet("A*");
-        Alphabet a1 = getNewAlphabet("A(");
-        Alphabet a2 = getNewAlphabet("A)");
-        if (a.contains('*') || a1.contains('(') || a2.contains(')')) {
-            throw new IllegalArgumentException("Alphabet can't contain *, (, " +
-                    "or ) characters");
-        }
-    }
-    @Test(expected = EnigmaException.class)
-    public void testInvalidCycleForm() {
-        Permutation p = getNewPermutation("((A))",
-                getNewAlphabet("AB"));
-    }
+//    @Test(expected = IllegalArgumentException.class)
+//    public void testNoAlphabet() {
+//        Permutation p = getNewPermutation("",
+//                getNewAlphabet(""));
+//        if (p.size()==0) {
+//            throw new IllegalArgumentException("No alphabet.");
+//        }
+//
+//    }
+//    @Test(expected = EnigmaException.class)
+//    public void testNotInAlphabet() {
+//        Permutation p = getNewPermutation("(ABCD)",
+//                getNewAlphabet("ABC"));
+//
+//    }
+//
+//    @Test(expected = EnigmaException.class)
+//    public void testInAlphabetTwice() {
+//        Permutation p = getNewPermutation("(ABD)",
+//                getNewAlphabet("ABBD"));
+//
+//    }
+//    @Test(expected = EnigmaException.class)
+//    public void testInCyclesTwice() {
+//        Permutation p = getNewPermutation("(ABBD)",
+//                getNewAlphabet("ABD"));
+//
+//    }
+//    @Test(expected = IllegalArgumentException.class)
+//    public void testInvalidLetter() {
+//        Alphabet a = getNewAlphabet("A*");
+//        Alphabet a1 = getNewAlphabet("A(");
+//        Alphabet a2 = getNewAlphabet("A)");
+//        if (a.contains('*') || a1.contains('(') || a2.contains(')')) {
+//            throw new IllegalArgumentException("Alphabet can't contain *, (, " +
+//                    "or ) characters");
+//        }
+//    }
+//    @Test(expected = EnigmaException.class)
+//    public void testInvalidCycleForm() {
+//        Permutation p = getNewPermutation("((A))",
+//                getNewAlphabet("AB"));
+//    }
 
 
 

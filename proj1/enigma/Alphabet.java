3e7e1dac1f13a2a096d802d661bd3fa7a62553ef
Pavel Gladkevich <pgladkevich@berkeley.edu>
2020-03-01 14:51:48 -0800

    Maybe fixed merge conflicts

--- proj1/enigma/Alphabet.java
+++ proj1/enigma/Alphabet.java
@@ -2,7 +2,7 @@ package enigma;
 
 /** An alphabet of encodable characters.  Provides a mapping from characters
  *  to and from indices into the alphabet.
- *  @author
+ *  @author Pavel Gladkevich
  */
 class Alphabet {
 

a2d542bf4bfb066ff5e057a6230296d730fe0d11
Pavel Gladkevich <pgladkevich@berkeley.edu>
2020-03-04 21:55:46 -0800

    Finished hw4

--- hw4/P2Pattern.java
+++ hw4/P2Pattern.java
@@ -11,12 +11,13 @@ public class P2Pattern {
     public static String P2 = "\\(([0-9]+)(, +[0-9]+)+(, +[0-9]+\\))";
 
     /* Pattern to match a valid domain name. Eg: www.support.facebook-login.com */
-    public static String P3 = ""; //FIXME: Add your regex here
+    public static String P3 = "[a-z]+(\\.[\\w-]+)+";
 
     /* Pattern to match a valid java variable name. Eg: _child13$ */
-    public static String P4 = ""; //FIXME: Add your regex here
+    public static String P4 = "[^0-9].*";
 
     /* Pattern to match a valid IPv4 address. Eg: 127.0.0.1 */
-    public static String P5 = "";; //FIXME: Add your regex here
+    public static String P5 = "(([01]([0-9]?){2}|2([0-5]?){2})" +
+            "\\.){3}([01]([0-9]?){2}|2([0-5]?){2})";
 
 }

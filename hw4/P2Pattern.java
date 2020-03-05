df44577793b532c244de00324ab293072c62a75a
Pavel Gladkevich <pgladkevich@berkeley.edu>
2020-03-04 21:19:56 -0800

    Finished 2 patterns

--- hw4/P2Pattern.java
+++ hw4/P2Pattern.java
@@ -4,10 +4,11 @@
 
 public class P2Pattern {
     /* Pattern to match a valid date of the form MM/DD/YYYY. Eg: 9/22/2019 */
-    public static String P1 = ""; //FIXME: Add your regex here
+    public static String P1 = "([0-9]|0[0-9]|1[0-2])\\/" +
+            "(0?[0-9]|[12][0-9]|3[0-1])\\/\\d{4}";
 
     /** Pattern to match 61b notation for literal IntLists. */
-    public static String P2 = ""; //FIXME: Add your regex here
+    public static String P2 = "\\(([0-9]+)(, +[0-9]+)+(, +[0-9]+\\))";
 
     /* Pattern to match a valid domain name. Eg: www.support.facebook-login.com */
     public static String P3 = ""; //FIXME: Add your regex here

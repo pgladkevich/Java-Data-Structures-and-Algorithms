2bf3b469dcf851c39aee7e295f16ee7da8acd201
Paul Hilfinger <Hilfinger@cs.berkeley.edu>
2020-03-12 00:50:49 -0700

    Update proj2

---
+++ proj2/loa/Board.java
@@ -0,0 +1,260 @@
+/* Skeleton Copyright (C) 2015, 2020 Paul N. Hilfinger and the Regents of the
+ * University of California.  All rights reserved. */
+package loa;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Formatter;
+import java.util.List;
+
+import java.util.regex.Pattern;
+
+import static loa.Piece.*;
+import static loa.Square.*;
+
+/** Represents the state of a game of Lines of Action.
+ *  @author
+ */
+class Board {
+
+    /** Default number of moves for each side that results in a draw. */
+    static final int DEFAULT_MOVE_LIMIT = 60;
+
+    /** Pattern describing a valid square designator (cr). */
+    static final Pattern ROW_COL = Pattern.compile("^[a-h][1-8]$");
+
+    /** A Board whose initial contents are taken from INITIALCONTENTS
+     *  and in which the player playing TURN is to move. The resulting
+     *  Board has
+     *        get(col, row) == INITIALCONTENTS[row][col]
+     *  Assumes that PLAYER is not null and INITIALCONTENTS is 8x8.
+     *
+     *  CAUTION: The natural written notation for arrays initializers puts
+     *  the BOTTOM row of INITIALCONTENTS at the top.
+     */
+    Board(Piece[][] initialContents, Piece turn) {
+        initialize(initialContents, turn);
+    }
+
+    /** A new board in the standard initial position. */
+    Board() {
+        this(INITIAL_PIECES, BP);
+    }
+
+    /** A Board whose initial contents and state are copied from
+     *  BOARD. */
+    Board(Board board) {
+        this();
+        copyFrom(board);
+    }
+
+    /** Set my state to CONTENTS with SIDE to move. */
+    void initialize(Piece[][] contents, Piece side) {
+        // FIXME
+        _turn = side;
+        _moveLimit = DEFAULT_MOVE_LIMIT;
+    }
+
+    /** Set me to the initial configuration. */
+    void clear() {
+        initialize(INITIAL_PIECES, BP);
+    }
+
+    /** Set my state to a copy of BOARD. */
+    void copyFrom(Board board) {
+        if (board == this) {
+            return;
+        }
+        // FIXME
+    }
+
+    /** Return the contents of the square at SQ. */
+    Piece get(Square sq) {
+        return _board[sq.index()];
+    }
+
+    /** Set the square at SQ to V and set the side that is to move next
+     *  to NEXT, if NEXT is not null. */
+    void set(Square sq, Piece v, Piece next) {
+        // FIXME
+    }
+
+    /** Set the square at SQ to V, without modifying the side that
+     *  moves next. */
+    void set(Square sq, Piece v) {
+        set(sq, v, null);
+    }
+
+    /** Set limit on number of moves (before tie results) to LIMIT. */
+    void setMoveLimit(int limit) {
+        _moveLimit = limit;
+        _winnerKnown = false;
+    }
+
+    /** Assuming isLegal(MOVE), make MOVE. Assumes MOVE.isCapture()
+     *  is false. */
+    void makeMove(Move move) {
+        assert isLegal(move);
+        // FIXME
+    }
+
+    /** Retract (unmake) one move, returning to the state immediately before
+     *  that move.  Requires that movesMade () > 0. */
+    void retract() {
+        assert movesMade() > 0;
+        // FIXME
+    }
+
+    /** Return the Piece representing who is next to move. */
+    Piece turn() {
+        return _turn;
+    }
+
+    /** Return true iff FROM - TO is a legal move for the player currently on
+     *  move. */
+    boolean isLegal(Square from, Square to) {
+        return true;   // FIXME
+    }
+
+    /** Return true iff MOVE is legal for the player currently on move.
+     *  The isCapture() property is ignored. */
+    boolean isLegal(Move move) {
+        return isLegal(move.getFrom(), move.getTo());
+    }
+
+    /** Return a sequence of all legal moves from this position. */
+    List<Move> legalMoves() {
+        return null;  // FIXME
+    }
+
+    /** Return true iff the game is over (either player has all his
+     *  pieces continguous or there is a tie). */
+    boolean gameOver() {
+        return winner() != null;
+    }
+
+    /** Return true iff SIDE's pieces are continguous. */
+    boolean piecesContiguous(Piece side) {
+        return getRegionSizes(side).size() == 1;
+    }
+
+    /** Return the winning side, if any.  If the game is not over, result is
+     *  null.  If the game has ended in a tie, returns EMP. */
+    Piece winner() {
+        if (!_winnerKnown) {
+            // FIXME
+            _winnerKnown = true;
+        }
+        return _winner;
+    }
+
+    /** Return the total number of moves that have been made (and not
+     *  retracted).  Each valid call to makeMove with a normal move increases
+     *  this number by 1. */
+    int movesMade() {
+        return _moves.size();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Board b = (Board) obj;
+        return Arrays.deepEquals(_board, b._board) && _turn == b._turn;
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(_board) * 2 + _turn.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        Formatter out = new Formatter();
+        out.format("===%n");
+        for (int r = BOARD_SIZE - 1; r >= 0; r -= 1) {
+            out.format("    ");
+            for (int c = 0; c < BOARD_SIZE; c += 1) {
+                out.format("%s ", get(sq(c, r)).abbrev());
+            }
+            out.format("%n");
+        }
+        out.format("Next move: %s%n===", turn().fullName());
+        return out.toString();
+    }
+
+    /** Return true if a move from FROM to TO is blocked by an opposing
+     *  piece or by a friendly piece on the target square. */
+    private boolean blocked(Square from, Square to) {
+        return false; // FALSE
+    }
+
+    /** Return the size of the as-yet unvisited cluster of squares
+     *  containing P at and adjacent to SQ.  VISITED indicates squares that
+     *  have already been processed or are in different clusters.  Update
+     *  VISITED to reflect squares counted. */
+    private int numContig(Square sq, boolean[][] visited, Piece p) {
+        return 0;  // FIXME
+    }
+
+    /** Set the values of _whiteRegionSizes and _blackRegionSizes. */
+    private void computeRegions() {
+        if (_subsetsInitialized) {
+            return;
+        }
+        _whiteRegionSizes.clear();
+        _blackRegionSizes.clear();
+        // FIXME
+        Collections.sort(_whiteRegionSizes, Collections.reverseOrder());
+        Collections.sort(_blackRegionSizes, Collections.reverseOrder());
+        _subsetsInitialized = true;
+    }
+
+    /** Return the sizes of all the regions in the current union-find
+     *  structure for side S. */
+    List<Integer> getRegionSizes(Piece s) {
+        computeRegions();
+        if (s == WP) {
+            return _whiteRegionSizes;
+        } else {
+            return _blackRegionSizes;
+        }
+    }
+
+    // FIXME: Other methods, variables?
+
+    /** The standard initial configuration for Lines of Action (bottom row
+     *  first). */
+    static final Piece[][] INITIAL_PIECES = {
+        { EMP, BP,  BP,  BP,  BP,  BP,  BP,  EMP },
+        { WP,  EMP, EMP, EMP, EMP, EMP, EMP, WP  },
+        { WP,  EMP, EMP, EMP, EMP, EMP, EMP, WP  },
+        { WP,  EMP, EMP, EMP, EMP, EMP, EMP, WP  },
+        { WP,  EMP, EMP, EMP, EMP, EMP, EMP, WP  },
+        { WP,  EMP, EMP, EMP, EMP, EMP, EMP, WP  },
+        { WP,  EMP, EMP, EMP, EMP, EMP, EMP, WP  },
+        { EMP, BP,  BP,  BP,  BP,  BP,  BP,  EMP }
+    };
+
+    /** Current contents of the board.  Square S is at _board[S.index()]. */
+    private final Piece[] _board = new Piece[BOARD_SIZE  * BOARD_SIZE];
+
+    /** List of all unretracted moves on this board, in order. */
+    private final ArrayList<Move> _moves = new ArrayList<>();
+    /** Current side on move. */
+    private Piece _turn;
+    /** Limit on number of moves before tie is declared.  */
+    private int _moveLimit;
+    /** True iff the value of _winner is known to be valid. */
+    private boolean _winnerKnown;
+    /** Cached value of the winner (BP, WP, EMP (for tie), or null (game still
+     *  in progress).  Use only if _winnerKnown. */
+    private Piece _winner;
+
+    /** True iff subsets computation is up-to-date. */
+    private boolean _subsetsInitialized;
+
+    /** List of the sizes of continguous clusters of pieces, by color. */
+    private final ArrayList<Integer>
+        _whiteRegionSizes = new ArrayList<>(),
+        _blackRegionSizes = new ArrayList<>();
+}
---
+++ proj2/loa/UnitTests.java
@@ -0,0 +1,27 @@
+/* Skeleton Copyright (C) 2015, 2020 Paul N. Hilfinger and the Regents of the
+ * University of California.  All rights reserved. */
+package loa;
+
+import ucb.junit.textui;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/** The suite of all JUnit tests for the loa package.
+ *  @author
+ */
+public class UnitTests {
+
+    /** Run the JUnit tests in the loa package. */
+    public static void main(String[] ignored) {
+        textui.runClasses(UnitTests.class);
+        textui.runClasses(BoardTest.class);
+    }
+
+    /** A dummy test to avoid complaint. */
+    @Test
+    public void placeholderTest() {
+    }
+
+}
+
+
